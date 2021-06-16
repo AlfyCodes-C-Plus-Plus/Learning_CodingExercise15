7ea6c3cebbc93c5515587360100075bde4e64229
AlfyCodes <Alfred.Ortiz1@marist.edu>
2021-06-15 23:42:01 -0400

    Coding Exercise Complete

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Coding Exercise 15
+
+Use a range-based for loop to loop through a given vector of integers and determine how many elements in the vector are evenly divisible by either 3 or by 5.
+
+The final result should be stored in an integer variable named count.
+
+The vector of integers has been provided for you and is named vec.
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+    vector<int> vec{1, 3, 5, 15, 16, 17, 18, 19, 20, 21, 25, 26, 27, 30, 50, 55, 56, 58, 100, 200, 300, 400, 500, 600, 700};
+    int count{};
+    for (auto vector : vec)
+    {
+
+        if ((vector % 3) == 0 || (vector % 5) == 0)
+        {
+            count++;
+        }
+    }
+
+    cout << "The count is: " << count;
+
+    return 0;
+}
